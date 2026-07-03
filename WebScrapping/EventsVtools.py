20d0ce369fdc5d43ab0751d6f650ce432a5c5f66
DasReyxr <contreras.orlandoxv@gmail.com>
2026-07-03 17:07:46 -0600

    Added WebScrapping VTools

---
+++ WebScrapping/EventsVtools.py
@@ -0,0 +1,48 @@
+events = [
+    {
+        "title": "WIE",
+        "category": "Technical",
+        # "subcategory": "Continuing Education",
+        "start_time": "09 Jul 2026 02:03 PM",
+        "end_time":   "24 Jul 2026 02:05 PM",
+        "timezone": "America/Mexico_City",
+        "description": "<p>Descripción del evento aquí.</p>",
+        "header": "<p>Encabezado</p>",
+        "footer": "<p>Pie de página</p>",
+        "agenda": "<p>Agenda</p>",
+        "keywords": "#IEEE, #evento, #tecnologia",
+        "survey_url": "https://ejemplo.com/encuesta",
+
+        "location": {
+            "type": "physical",  # physical | virtual | hybrid
+            "address1": "Av. Universidad 940",
+            "city": "Aguascalientes",
+            "postal_code": "20131",
+            "building": "Centro de Ciencias Básicas",
+            "country": "Mexico",
+            # "state": "Aguascalientes",
+        },
+    },
+    {
+        "title": "Otro evento",
+        "category": "Technical",
+        "start_time": "01 Aug 2026 09:00 AM",
+        "end_time": "01 Aug 2026 01:00 PM",
+        "timezone": "America/Mexico_City",
+        "description": "<p>Descripción del segundo evento.</p>",
+        "header": "<p>Encabezado</p>",
+        "footer": "<p>Pie de página</p>",
+        "agenda": "<p>Agenda</p>",
+        "keywords": "IEEE workshop",
+        "survey_url": "https://ejemplo.com/survey",
+
+        "location": {
+            "type": "physical",
+            "address1": "Av. Universidad 940",
+            "city": "Aguascalientes",
+            "postal_code": "20131",
+            "building": "Centro de Ciencias Básicas",
+            "country": "Mexico",
+        },
+    },
+]
